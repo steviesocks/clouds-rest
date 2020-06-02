7e9f8337429efa5346340c6f571a2639eee0e722
steviesocks <stephen.stocking@gmail.com>
2020-06-02 16:44:56 -0400

    update menu items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,32 +10,32 @@ class Directory extends Component {
             sections: [
                 {
                   title: 'boots',
-                  imageUrl: 'https://www.salomon.com/sites/default/files/styles/crop_link_ratio/public/httr/2019-08/Header-how-to-choose-hiking-shoes-boots.jpg?itok=u6hJuax8',
+                  imageUrl: 'https://i.ibb.co/0YTqWd1/Header-how-to-choose-hiking-shoes-boots.jpg',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'boots'
                 },
                 {
                   title: 'tents',
-                  imageUrl: 'https://cdn.hiconsumption.com/wp-content/uploads/2015/05/Best-Backpacking-Tents.jpg',
+                  imageUrl: 'https://i.ibb.co/cTrxQ81/Best-Backpacking-Tents.jpg',
                   id: 2,
                   linkUrl: 'shop/jackets'
                 },
                 {
                   title: 'backpacks',
-                  imageUrl: 'https://gotonationalparks.com/wp-content/uploads/2019/02/10-Best-Ultralight-Backpacks.jpg',
+                  imageUrl: 'https://i.ibb.co/q0pKkvL/10-Best-Ultralight-Backpacks.jpg',
                   id: 3,
                   linkUrl: 'shop/sneakers'
                 },
                 {
                   title: "women's",
-                  imageUrl: 'https://s3.amazonaws.com/images.gearjunkie.com/uploads/2018/03/piccourtesyfjallraven-bestwomenshikingpants.jpg',
+                  imageUrl: 'https://i.ibb.co/4pcBYm3/piccourtesyfjallraven-bestwomenshikingpants.jpg',
                   size: 'large',
                   id: 4,
                   linkUrl: 'shop/womens'
                 },
                 {
                   title: "men's",
-                  imageUrl: 'https://contents.mediadecathlon.com/p1525110/k$258ff5ef33d9b954de13079b7034dfff/mens-mountain-trekking-waterproof-convertible-trousers-trek500-blue.jpg?&f=800x800',
+                  imageUrl: 'https://i.ibb.co/48RTFKq/mens-mountain-trekking-waterproof-convertible-trousers-trek500-blue.jpg',
                   size: 'large',
                   id: 5,
                   linkUrl: 'shop/mens'
@@ -48,8 +48,8 @@ class Directory extends Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size})=> (
-                        <MenuItem  key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps})=> (
+                        <MenuItem  key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
