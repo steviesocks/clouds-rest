aed02b80e5962f7ed1ccf7c1fadbc38044d564dc
steviesocks <stephen.stocking@gmail.com>
2020-10-09 13:49:39 -0400

    separating cra app into client folder, added express server to handle backend stripe payment

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+import logo from '../../assets/clouds-rest-logo.png';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceInCents = price * 100;
+    const publishableKey = 'pk_test_51HJKL7E9WVC2Ygw4zc2AkYFndCRO53n7BPuaIbg2tA63otNp6oAdhJ4aYGKtHtxPQLVNL9wFN8o6WXDjoyn7QiBm00Mek8TeaH';
+
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceInCents,
+                token
+            }
+        }).then(response => {
+            alert('Test Payment Successful!')
+        }).catch(error => {
+            console.log('payment error', JSON.parse(error));
+            alert(
+                'There was an issue with your test payment. Please use the test card provided.'
+            )
+        })
+    }
+
+    return (
+        <StripeCheckout className='stripe-checkout'
+            label='Pay With Card'
+            name='Clouds Rest Outfitters'
+            billingAddress
+            shippingAddress
+            image={logo}
+            description={`Your total is $${price}`}
+            amount={priceInCents}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+
+        />
+    );
+};
+
+export default StripeCheckoutButton;
